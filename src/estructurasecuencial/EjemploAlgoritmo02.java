65c1fc529940cbe9d1782293e9fc876f5a3f0a8f
EdderCodarlupo <edder.codarlupo@gmail.com>
2024-11-13 12:38:43 -0500

    subiendo cambios desde la rama Rene

---
+++ src/estructurasecuencial/EjemploAlgoritmo02.java
@@ -0,0 +1,16 @@
+
+package estructurasecuencial;
+
+/**
+ *
+ * @author Edder
+ */
+public class EjemploAlgoritmo02 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Estoy haciendo cambio desde la ramaRene :D");
+        
+    }
+    
+}
