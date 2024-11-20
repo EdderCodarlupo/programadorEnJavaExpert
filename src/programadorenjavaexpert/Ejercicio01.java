db8d48d214dd91541cf835e63924a109bfdcb570
renejose <renejose6543@gmmail.com>
2024-11-20 10:28:38 -0500

    cambios

--- src/programadorenjavaexpert/Ejercicio01.java
+++ src/programadorenjavaexpert/Ejercicio01.java
@@ -8,14 +8,22 @@ public class Ejercicio01 {
     
     public static void main(String[] args) {
         
-        //Variables
-        int numeroPersonas = 5; 
-        int totalApretonesMano;
+        final double IGV = 0.18;
         
+        //Variables
+        int numeroPersonas = 5,totalApretonesMano =0; 
+        
+        int num = totalApretonesMano + 1 ;
         //Proceso
-        totalApretonesMano = numeroPersonas * (numeroPersonas - 1 ) / 2;
+        //totalApretonesMano = numeroPersonas * (numeroPersonas - 1 ) / 2;
+        
+        totalApretonesMano = (numeroPersonas + 3 ) ;
         
         //Salida
+        System.out.println(totalApretonesMano);
+        
+        totalApretonesMano = totalApretonesMano / num;
+        
         System.out.println(totalApretonesMano);
         
     }
